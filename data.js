a5ef4382e54792e4360fdd41970cb68128773be6
marathongodsheet <marathongodsheet@gmail.com>
2026-06-04 02:12:34 -0400

    Add files via upload
    Upload web files

---
+++ data.js
@@ -0,0 +1,179 @@
+window.INITIAL_DATA = [
+  {
+    "category": "Grey",
+    "items": [
+      {
+        "name": "Biomass",
+        "total": 169
+      },
+      {
+        "name": "Gunmetal",
+        "total": 126
+      },
+      {
+        "name": "Diodes",
+        "total": 216
+      },
+      {
+        "name": "Lead",
+        "total": 229
+      },
+      {
+        "name": "Gel",
+        "total": 356
+      }
+    ]
+  },
+  {
+    "category": "Green",
+    "items": [
+      {
+        "name": "Sparkleaf",
+        "total": 86
+      },
+      {
+        "name": "Reclaimed Biostripping",
+        "total": 90
+      },
+      {
+        "name": "Dermachem Pack",
+        "total": 17
+      },
+      {
+        "name": "Plasma Filament",
+        "total": 35
+      },
+      {
+        "name": "Deimosite Rods",
+        "total": 46
+      },
+      {
+        "name": "Altered Wire",
+        "total": 16
+      },
+      {
+        "name": "Dynamic Compounds",
+        "total": 132
+      },
+      {
+        "name": "Surveillance Lens",
+        "total": 109
+      },
+      {
+        "name": "Drone Node",
+        "total": 182
+      },
+      {
+        "name": "Drone Resin",
+        "total": 127
+      },
+      {
+        "name": "Storage Drive",
+        "total": 147
+      },
+      {
+        "name": "Fractal Circuit",
+        "total": 69
+      }
+    ]
+  },
+  {
+    "category": "Blue",
+    "items": [
+      {
+        "name": "Tarax Seed",
+        "total": 41
+      },
+      {
+        "name": "Neurochem Pack",
+        "total": 58
+      },
+      {
+        "name": "Strerilized Biostripping",
+        "total": 37
+      },
+      {
+        "name": "Anomalous Wire",
+        "total": 15
+      },
+      {
+        "name": "Tachyon Filament",
+        "total": 12
+      },
+      {
+        "name": "Cetinite Rods",
+        "total": 6
+      },
+      {
+        "name": "Thoughtwave Lens",
+        "total": 53
+      },
+      {
+        "name": "Volatile Compunds",
+        "total": 68
+      },
+      {
+        "name": "Biomata Node",
+        "total": 84
+      },
+      {
+        "name": "Biomata Resin",
+        "total": 60
+      },
+      {
+        "name": "Paradox Circuit",
+        "total": 46
+      },
+      {
+        "name": "Amygdala Drive",
+        "total": 22
+      }
+    ]
+  },
+  {
+    "category": "Purple",
+    "items": [
+      {
+        "name": "Biolens Seed",
+        "total": 3
+      },
+      {
+        "name": "Nueral Insulation",
+        "total": 2
+      },
+      {
+        "name": "Predictive Framework",
+        "total": 2
+      },
+      {
+        "name": "Ballistic Turbine",
+        "total": 2
+      },
+      {
+        "name": "Enzyme Replicator",
+        "total": 1
+      },
+      {
+        "name": "Reflex Coil",
+        "total": 2
+      }
+    ]
+  },
+  {
+    "category": "Gold",
+    "items": [
+      {
+        "name": "Alien Alloy",
+        "total": 0
+      },
+      {
+        "name": "Hazard Capsule",
+        "total": 0
+      },
+      {
+        "name": "Synapse Cube",
+        "total": 0
+      }
+    ]
+  }
+];
